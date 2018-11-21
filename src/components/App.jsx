b66884d4789b2751209db8ab2c22304ed88248b4
ira strikun <ira.strikun@gmail.com>
2018-11-21 17:00:46 +0200

    Start genres filter

---
+++ src/components/App.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Filters from "./Filters/Filters";
+import MoviesList from "./Movies/MoviesList";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filters: {
+        sort_by: "popularity.desc",
+        primary_release_year: "2018",
+        with_genres: []
+      },
+      page: 1
+    };
+  }
+
+  onChangeFilters = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState(prevState => ({
+      filters: {
+        ...prevState.filters,
+        [name]: value
+      }
+    }));
+  };
+
+  onChangePage = page => {
+    this.setState({
+      // page: page
+      page
+    });
+  };
+
+  render() {
+    const { filters, page, with_genres } = this.state;
+    return (
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-4">
+            <div className="card" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h3>Фильтры:</h3>
+                <Filters
+                  page={page}
+                  filters={filters}
+                  with_genres={with_genres}
+                  onChangeFilters={this.onChangeFilters}
+                  onChangePage={this.onChangePage}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-8">
+            <MoviesList
+              filters={filters}
+              page={page}
+              onChangePage={this.onChangePage}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import SortBy from "./SortBy";
+import PrimaryReleaseYear from "./PrimaryReleaseYear"
+import Genres from "./Genres"
+
+export default class Filters extends React.Component {
+  render() {
+    const {
+      filters: { sort_by, primary_release_year, with_genres },
+      page,
+      onChangeFilters,
+      onChangePage
+    } = this.props;
+    return (
+      <form className="mb-3">
+        <SortBy sort_by={sort_by} onChangeFilters={onChangeFilters} />
+        <PrimaryReleaseYear primary_release_year={primary_release_year} onChangeFilters={onChangeFilters} />
+        <Genres with_genres={with_genres} onChangeFilters={onChangeFilters} />
+
+
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-light"
+            disabled={page === 1}
+            onClick={onChangePage.bind(null, page - 1)}
+          >
+            Назад
+          </button>
+          <button
+            type="button"
+            className="btn btn-light"
+            onClick={onChangePage.bind(null, page + 1)}
+          >
+            Вперед
+          </button>
+        </div>
+      </form>
+
+    );
+  }
+}
